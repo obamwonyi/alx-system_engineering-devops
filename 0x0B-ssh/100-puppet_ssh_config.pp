1bf02fa66888ee722c77463aabe61f5754214b7f
obamwonyi <developer.oduwa@gmail.com>
2024-02-25 19:54:48 +0100

    fixed task 4 error

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -3,17 +3,17 @@
 # Using puppet for connection without password
 
 file { '/etc/ssh/ssh_config':
-	ensure -> present,
+ ensure -> present,
 }
 
 file_line {	'Turn off passwd auth':
-	path => '/etc/ssh/ssh_config',
-	line => '/passwordAuthentication no',
-	match => '^#PasswordAuthentication',
+ path => '/etc/ssh/ssh_config',
+ line => '/passwordAuthentication no',
+ match => '^#PasswordAuthentication',
 }
 
 file line { 'Declare identity file':
-	path => '/etc/ssh/ssh_config',
-	line => 'IdentityFile ~/.ssh/school',
-	match => '^#IdentityFile',
+ path => '/etc/ssh/ssh_config',
+ line => 'IdentityFile ~/.ssh/school',
+ match => '^#IdentityFile',
 }
