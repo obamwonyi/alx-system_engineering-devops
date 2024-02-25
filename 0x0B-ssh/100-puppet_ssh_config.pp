62a22c94e7b01b849a91addc25261fec49a04f0e
obamwonyi <developer.oduwa@gmail.com>
2024-02-25 19:52:13 +0100

    added task 4

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+# Using puppet for connection without password
+
+file { '/etc/ssh/ssh_config':
+	ensure -> present,
+}
+
+file_line {	'Turn off passwd auth':
+	path => '/etc/ssh/ssh_config',
+	line => '/passwordAuthentication no',
+	match => '^#PasswordAuthentication',
+}
+
+file line { 'Declare identity file':
+	path => '/etc/ssh/ssh_config',
+	line => 'IdentityFile ~/.ssh/school',
+	match => '^#IdentityFile',
+}
